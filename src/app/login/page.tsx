4342bd0b6e6ecfebb2c8c9398982da13bb7f77df
inventahqlimited-creator <inventahqlimited@gmail.com>
2026-09-08 15:51:53 +1200

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,8 @@ import LoginForm from './login-form'
 
 export const metadata: Metadata = { title: 'Sign in' }
 
+export const dynamic = 'force-dynamic'
+
 export default function LoginPage() {
   return <LoginForm />
 }

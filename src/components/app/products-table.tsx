4da48f92002e3fb6f493aad7c368fece9f7bc805
inventahqlimited-creator <inventahqlimited@gmail.com>
2026-09-16 16:23:36 +1200

    Update products-table.tsx

--- src/components/app/products-table.tsx
+++ src/components/app/products-table.tsx
@@ -486,9 +486,7 @@ export default function ProductsTable({
                 </div>
               )}
             </div>
-              )}
-            </div>
-            <button className="btn btn-primary" onClick={openAdd}>
+              <button className="btn btn-primary" onClick={openAdd}>
               <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
               Add Product
             </button>
